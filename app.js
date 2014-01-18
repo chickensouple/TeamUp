84419c31c5e53aa3f83388f4df61c573d097844f
Ubuntu <ubuntu@ip-172-31-33-155.us-west-2.compute.internal>
2014-01-18 23:59:35 +0000

    fdsafdsafdsafd

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var http = require('http');
 var path = require('path');
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = monk('localhost:27017/teamup');
 
 var app = express();
 
