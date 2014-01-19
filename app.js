40fca0ef25a0e610bf705286b9066751376a351a
Denny Zhang <dennyz@umich.edu>
2014-01-19 02:20:02 -0500

    fdsa

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ var userbase = db.get('teamup.users');
 var teambase = db.get('teamup.teams');
 
 app.post('/', function(req, res){
-	console.log(req.body);
 	console.log(req.body.username);
 	console.log(req.body.userpass);	
 
